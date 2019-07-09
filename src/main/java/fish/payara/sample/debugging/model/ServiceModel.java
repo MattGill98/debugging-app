efe453360b71ea1e706a6223c03efececcf8f459
Matt Gill <matthew.gill@live.co.uk>
2019-07-09 12:02:56 +0100

    Added service model javadoc.

--- src/main/java/fish/payara/sample/debugging/model/ServiceModel.java
+++ src/main/java/fish/payara/sample/debugging/model/ServiceModel.java
@@ -8,6 +8,9 @@ import java.util.LinkedHashSet;
 import javax.enterprise.context.ApplicationScoped;
 
 /**
+ * A class to act as a temporary database for the application and provide
+ * convenience methods for accessing the data.
+ * 
  * @author Matt Gill
  */
 @ApplicationScoped
@@ -21,19 +24,26 @@ public class ServiceModel {
         this.libraries = new LinkedHashSet<>();
     }
 
+    /**
+     * @return all books across all libraries.
+     */
     public Collection<Book> getBooks() {
         return unmodifiableCollection(books);
     }
 
+    /**
+     * @return all libraries.
+     */
     public Collection<Library> getLibraries() {
         return libraries;
     }
 
-    public Book addBook(Library library, Book book) {
-        library.addBook(book);
-        return addBook(book);
-    }
-
+    /**
+     * Adds a book to the store.
+     * 
+     * @param book the book to add.
+     * @return the added book.
+     */
     public Book addBook(Book book) {
         books.add(book);
         return book;
